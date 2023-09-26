aa2c5b16f42f254c835272feff923863388663ae
mucunguzi256 <moemucu@gmail.com>
2023-09-26 06:19:03 +0300

    additions

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // set heade content type
-  res.setHeader('content-type', 'text/plain');
-  res.write('Hello, am Mucu');
-  res.end();
+  res.setHeader('content-type', 'text/html');
+
+  // send an html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //res.write(data);
+      res.end(data);
+    }
+  });
 });
 server.listen(3000, 'localhost', () => {
   console.log('Listening to requests on port 3000');

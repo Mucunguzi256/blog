0e8c33ffc294713b92972f4ac12d09990723e292
mucunguzi256 <moemucu@gmail.com>
2023-09-28 21:41:54 +0300

    Remove node_modules from git

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const express = require('express');
 
 const app = express();
 
+// view engine
+
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
 	//res.send('<p> Home Page </p>');
-	res.sendFile('./views/index.html', { root: __dirname });
+	res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
 	//res.send('<p> Home Page </p>');
-	res.sendFile('./views/about.html', { root: __dirname });
+	res.render('about', { title: 'About' });
 });
 
-//Redrict
-app.get('/about-us', (req, res) => {
-	res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+	res.render('create', { title: 'Create a new blog' });
 });
 
 //404
 app.use((req, res) => {
-	res.sendFile('./views/404.html', { root: __dirname });
+	res.status(404).render('404', { title: '404' });
 });

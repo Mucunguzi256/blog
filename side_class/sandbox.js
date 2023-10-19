46cbef8352f5dc2574809da18eeeb2db3893908e
mucunguzi256 <moemucu@gmail.com>
2023-10-19 21:25:03 +0300

    js extras

---
+++ side_class/sandbox.js
@@ -0,0 +1,10 @@
+// Loops
+for (let i = 0; i < 5; i++) {
+  consele.log('in loop' + i);
+}
+const animals = ['hen', 'cow', 'dog', 'sheep'];
+let i = 0;
+while (i < animals.length) {
+  console.log(animals[1]);
+  i++;
+}
